29c0cd5b06301f0af8da01f668ef56084b7fe9e8
Dirk Heilig <dicky.dicky@gmail.com>
2016-12-06 20:22:06 +0100

    init

---
+++ Solution/Day2/Part2.php
@@ -0,0 +1,9 @@
+<?php
+namespace AdventOfCode\Solution\Day2;
+
+
+class Part2 extends Part1
+{
+    protected $input;
+    protected $positionType = 'AdventOfCode\Solution\Day2\Position1_D';
+}
---
+++ stubs/Part2.php
@@ -0,0 +1,13 @@
+<?php
+namespace AdventOfCode\Solution\Day{DAY};
+class Part2{
+    protected $input;
+    public function __construct($input)
+    {
+        $this->input = $input;
+    }
+    public function solve(){
+
+        return "this is not the right answer.\n";
+    }
+}
